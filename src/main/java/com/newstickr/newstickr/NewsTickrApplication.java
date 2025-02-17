5ce9b88dcb5b69bbf1dcf6730da0c19fb45c7f73
DongHeonKwak <112706887+Honey0v@users.noreply.github.com>
2025-02-17 18:10:49 +0900

    init : 프로젝트 초기 설정
    프로젝트 초기 설정

---
+++ src/main/java/com/newstickr/newstickr/NewsTickrApplication.java
@@ -0,0 +1,13 @@
+package com.newstickr.newstickr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsTickrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsTickrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newstickr/newstickr/NewsTickrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstickr.newstickr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsTickrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
