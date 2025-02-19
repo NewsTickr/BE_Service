763c5cda4f90894f29e8c6d0e4106554680c3bdd
Yunsu <pianoest@naver.com>
2025-02-19 21:17:00 +0900

    feat: User + UserRepository

---
+++ src/main/java/com/newstickr/newstickr/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.newstickr.newstickr.repository;
+
+import com.newstickr.newstickr.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
