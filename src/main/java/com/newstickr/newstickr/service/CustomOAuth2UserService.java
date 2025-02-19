caef52c3590c49a1664956853b26f70ec285e748
Yunsu <pianoest@naver.com>
2025-02-19 21:05:08 +0900

    feat: OAuth2 사용자 서비스 CustomOAuth2UserService 추가

---
+++ src/main/java/com/newstickr/newstickr/service/CustomOAuth2UserService.java
@@ -0,0 +1,80 @@
+package com.newstickr.newstickr.service;
+
+import com.newstickr.newstickr.dto.CustomOAuth2User;
+import com.newstickr.newstickr.dto.NaverResponse;
+import com.newstickr.newstickr.dto.OAuth2Response;
+import com.newstickr.newstickr.dto.UserDTO;
+import com.newstickr.newstickr.entity.User;
+import com.newstickr.newstickr.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println(oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2Response oAuth2Response = null; // 인터페이스 바구니 생성,,
+
+        if (registrationId.equals("naver")) {
+            // 네이버의 사용자 정보(JSON)를 Map<String, Object> 형태로 변환 -> 객체로 변환
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else {
+            return null;
+        }
+
+        // 네이버가 OAuth2 인증 시 제공하는 사용자의 고유 ID를 반환
+        String username = oAuth2Response.getProviderId();
+        User existData = userRepository.findByUsername(username);
+
+        if (existData == null) {
+
+            User userEntity = new User();
+
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
+    }
+}
