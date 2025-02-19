122bf8f3606b6dc4d94508faadce48a28d99ba80
Yunsu <pianoest@naver.com>
2025-02-19 21:14:38 +0900

    feat: OAuth2 로그인 성공 핸들러

---
+++ src/main/java/com/newstickr/newstickr/oauth2/CustomSuccessHandler.java
@@ -0,0 +1,60 @@
+package com.newstickr.newstickr.oauth2;
+
+import com.newstickr.newstickr.dto.CustomOAuth2User;
+import com.newstickr.newstickr.jwt.JWTUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+@Component
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
+
+    public CustomSuccessHandler(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        //OAuth2User
+        CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
+
+        // username
+        String password = customUserDetails.getPassword();
+
+        // role
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+        String role = auth.getAuthority();
+
+        // token = username + role
+        String token = jwtUtil.createJwt(password, role, 60*60*60L);
+
+        // 쿠키 방식 전달 + 프론트에 리다이렉트
+        response.addCookie(createCookie("Authorization", token));
+        response.sendRedirect("http://localhost:3000/");
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(60*60*60);
+        //cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+}
