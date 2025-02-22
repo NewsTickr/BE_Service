f9579b676c68ddc456e53642ebb1025901a98bb6
광부입니다 <kwy1379@ajou.ac.kr>
2025-02-23 02:01:18 +0900

    Fix: CommentController에서 SecurityContext를 사용하도록 수정, Swagger 내용 추가 Feat: 회원정보 API 추가, 회원 기본 이미지 추가

---
+++ src/main/java/com/newstickr/newstickr/user/service/FileService.java
@@ -0,0 +1,81 @@
+package com.newstickr.newstickr.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class FileService {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+//    private final String defaultImgUrl = "src/main/resources/static/defaultProfileImg.svg";
+
+    public Resource getImg(String fileName){
+        try{
+            Path path = Paths.get(uploadDir + fileName);
+            Resource resource = new UrlResource(path.toUri());
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            }
+        }catch (Exception e){
+            // [TODO]UrlResource에서 발생하는 예외 분석 필요.
+            log.error(e.getMessage());
+        }
+        return getDefaultImage();
+    }
+    public ResponseEntity<Resource> downloadImg(String fileName){
+        Resource resource = getImg(fileName);
+
+        if(resource.exists() || resource.isReadable()) {
+            String contentDisposition = "attachment; filename=\""+resource.getFilename()+"\"";
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                    .body(resource);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    public String uploadImg(MultipartFile file) {
+        //확장자는 자유 추후 결정
+        String fileExtension = getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
+        String fileName = UUID.randomUUID() + "." + fileExtension;
+        String filePath = uploadDir + fileName;
+        File targetFile = new File(filePath);
+        if(!targetFile.getParentFile().exists()){
+            targetFile.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(targetFile);
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return null;
+        }
+        return fileName;
+    }
+
+    private String getFileExtension(String fileName){
+        int dotIndex = fileName.lastIndexOf('.');
+        return dotIndex > 0 ? fileName.substring(dotIndex) : "";
+    }
+
+    private Resource getDefaultImage(){
+        try{
+            return new ClassPathResource("static/defaultProfileImg.svg");
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+}
