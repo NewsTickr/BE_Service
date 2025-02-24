21b9251ed73179b541843c2bbf53c69aa496ce22
Yunsu <pianoest@naver.com>
2025-02-24 11:19:06 +0900

    fix: JwtUtil 만료 수정

--- src/main/java/com/newstickr/newstickr/security/jwt/JWTUtil.java
+++ src/main/java/com/newstickr/newstickr/security/jwt/JWTUtil.java
@@ -1,6 +1,7 @@
 package com.newstickr.newstickr.security.jwt;
 
 import com.newstickr.newstickr.user.enums.Role;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -61,7 +62,11 @@ public class JWTUtil {
     }
 
     public Boolean isExpired(String token) {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        try {
+            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        }catch (ExpiredJwtException e){
+            return false;
+        }
     }
 
     public String createJwt(String username, String role, String id, Long expiredMs) {
